2c15d51ce201545283355eb041f4caa5e63e15e8
felix <Felix0417>
2023-08-07 19:11:36 +0500

    Initial commit

--- src/main/java/ru/javaops/topjava2/VotingServiceApplication.java
+++ src/main/java/ru/javaops/topjava2/VotingServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+public class VotingServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestaurantVotingApplication.class, args);
+        SpringApplication.run(VotingServiceApplication.class, args);
     }
 }
